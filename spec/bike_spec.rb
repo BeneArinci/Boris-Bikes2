568b352f9330a60449ab32b5f19409aca3ff7b6b
Student <student@makerss-air-2.mynet>
2020-05-13 15:05:04 +0100

    step 10 completed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,4 +5,8 @@ describe Bike do
   it 'working?' do
     expect(Bike.new).to respond_to (:working?)
   end
+
+  it 'working?' do
+    expect(Bike.new.working?).to eq(true)
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-
   it 'release_bike' do
-
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
   end
+
 end
